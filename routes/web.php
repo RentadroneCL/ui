84881df243a23e76c08346fd8f569db351182f04
Alvaro Farias <alvaro.farias@protonmail.com>
2020-05-07 23:34:07 -0400

    🚦 update web routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/project', 'ProjectController');
+
+Route::get('/', 'ProjectController@index');
+
+Route::post('/project/{project}/media/upload', 'UploadMedia')->name('media.upload');
