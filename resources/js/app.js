4ab5e9fb9f3df4b8e84132b9593d594fc3ff23df
Alvaro Farias <alvaro.farias@protonmail.com>
2020-05-07 23:35:06 -0400

    :bento: update assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+import Vue from 'vue';
+
+window.Vue = Vue;
+
+import UploadComponent from './components/UploadComponent';
+
+Vue.component('upload-component', UploadComponent);
+
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+
+const application = Application.start();
+const context = require.context('./controllers', true, /\.js$/);
+application.load(definitionsFromContext(context));
