374fd86e31af176e2edf5571dd7bf2f0214d6cef
Alvaro Farias <alvaro.farias@protonmail.com>
2020-05-11 19:33:00 -0400

    🚦 update routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('/project', 'ProjectController');
 
-Route::get('/', 'ProjectController@index');
+Route::get('/', 'ProjectController@index')->name('project.index');
 
 Route::post('/project/{project}/media/upload', 'UploadMedia')->name('media.upload');
