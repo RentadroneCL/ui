f08cea8edc04a7c51dfaf81511bace4a4d1536e5
Alvaro Farias <alvaro.farias@protonmail.com>
2020-06-20 23:38:59 -0400

    :art: update view

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,28 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>UI</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>UI</title>
 
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    <script src="{{ mix('js/app.js') }}" importance="high"></script>
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 </head>
 <body class="bg-blue-800">
   <div id="app" class="container bg-white tracking-wider text-gray-700 mx-auto mt-12 p-6 rounded-lg shadow-lg">
     @yield('content')
   </div>
+  <script src="{{ mix('js/app.js') }}"></script>
 </body>
-<script>
-  /**
-   * Next, we will create a fresh Vue application instance and attach it to
-   * the page. Then, you may begin adding components to this application
-   * or customize the JavaScript scaffolding to fit your unique needs.
-   */
-  const app = new Vue({
-    el: '#app'
-  });
-</script>
 </html>
